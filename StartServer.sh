7edf69e1b822b0838ff7a43c1316401c5040377c
Jesper Lykke <62839593+Lykkedk@users.noreply.github.com>
2020-12-22 11:42:06 +0100

    Delete StartServer.sh

--- StartServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Script to start the CamillaDSP server  192.168.1.XX:5000 (with correct IP address)
-
-su tc -c "/usr/local/bin/python3 /mnt/mmcblk0p2/tce/Camilla_Extensions/camillagui/main.py" &
-
-exit
